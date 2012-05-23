0c64ab0824036da49d20d8082739c7dea568a1a6
JudeArasu <judearasu@gmail.com>
2012-05-23 22:58:36 +0530

    copying the specs from the default strategy

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
 $:.unshift File.expand_path('..', __FILE__)
 $:.unshift File.expand_path('../../lib', __FILE__)
+require 'simplecov'
+SimpleCov.start
+require 'rspec'
+require 'rack/test'
+require 'webmock/rspec'
+require 'omniauth'
+require 'omniauth-mixi'
