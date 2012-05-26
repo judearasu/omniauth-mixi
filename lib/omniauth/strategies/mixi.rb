b410db44b0012cbc124375e145e2013f8d922b9a
JudeArasu <judearasu@gmail.com>
2012-05-26 13:50:25 +0530

    mixi api config

--- lib/omniauth/strategies/mixi.rb
+++ lib/omniauth/strategies/mixi.rb
@@ -9,6 +9,22 @@ module OmniAuth
                               :authorize_url => 'https://mixi.jp/connect_authorize.pl',
                               :access_token_url => 'https://secure.mixi-platform.com/2/token'
                               }
+                              
+       option :fields, ["id", "nickname","profileUrl","thumbnailUrl"]
+       uid { access_token.params[:user_id] }
+       
+        info do
+        {
+          :nickname => raw_info['nickname'],
+          :image => raw_info['profileUrl'],
+          :urls => {'public_profile' => raw_info['thumbnailUrl']}
+        }
+      end
+      
+       extra do
+        { 'raw_info' => raw_info }
+      end
+      
     end
   end
 end
