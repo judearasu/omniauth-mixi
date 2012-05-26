c1ef4d04c54e02db8815ed6d570dbdbaf1f82b53
JudeArasu <judearasu@gmail.com>
2012-05-26 12:15:47 +0530

    site specs

--- lib/omniauth/strategies/mixi.rb
+++ lib/omniauth/strategies/mixi.rb
@@ -1,9 +1,11 @@
 require 'omniauth-oauth2'
 require 'multi_json'
+
 module OmniAuth
   module Strategies
     class Mixi < OmniAuth::Strategies::OAuth2
-       
+      option :name, 'mixi'
+      option :client_options,{:site => 'http://api.mixi-platform.com/'}
     end
   end
 end
--- spec/omniauth/strategies/mixi_spec.rb
+++ spec/omniauth/strategies/mixi_spec.rb
@@ -4,11 +4,17 @@ describe OmniAuth::Strategies::Mixi do
   subject do
     OmniAuth::Strategies::Mixi.new({})
   end
-  
+
   context "client options" do
     it 'should have correct name' do
       subject.options.name.should eq("mixi")
     end
+
+    it 'should have correct site' do
+      subject.options.client_options.site.should eq("http://api.mixi-platform.com/")
+    end
+
+    
   end
 
 end
