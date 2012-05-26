1bb23fd1b334f3abe845919fa40f502f1e7211ab
JudeArasu <judearasu@gmail.com>
2012-05-26 13:07:25 +0530

    specs for token url

--- spec/omniauth/strategies/mixi_spec.rb
+++ spec/omniauth/strategies/mixi_spec.rb
@@ -11,9 +11,16 @@ describe OmniAuth::Strategies::Mixi do
     end
 
     it 'should have correct site' do
-      subject.options.client_options.site.should eq("http://api.mixi-platform.com/")
+      subject.options.client_options.site.should eq('http://api.mixi-platform.com/')
     end
 
+    it 'should have authorize url' do
+      subject.options.client_options.authorize_url eq('https://mixi.jp/connect_authorize.pl')
+    end
+    
+    it 'should have access token url' do
+      subject.options.client_options.access_token_url eq('https://secure.mixi-platform.com/2/token')
+    end
     
   end
 
