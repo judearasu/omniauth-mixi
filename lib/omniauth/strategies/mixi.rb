aab1ce59e8d7f03a29c0c10c01b5a606073d6804
JudeArasu <judearasu@gmail.com>
2012-05-26 08:48:10 +0530

    using oauth2

--- lib/omniauth/strategies/mixi.rb
+++ lib/omniauth/strategies/mixi.rb
@@ -1,8 +1,8 @@
-require 'omniauth-oauth'
+require 'omniauth-oauth2'
 require 'multi_json'
 module OmniAuth
   module Strategies
-    class Mixi < OmniAuth::Strategies::OAuth
+    class Mixi < OmniAuth::Strategies::OAuth2
       
     end
   end
