2aa8bb95dc7273e4e49ea11328fed339e0cd5927
JudeArasu <judearasu@gmail.com>
2012-05-26 13:14:30 +0530

    travis fails

--- lib/omniauth/strategies/mixi.rb
+++ lib/omniauth/strategies/mixi.rb
@@ -5,7 +5,10 @@ module OmniAuth
   module Strategies
     class Mixi < OmniAuth::Strategies::OAuth2
       option :name, 'mixi'
-      option :client_options,{:site => 'http://api.mixi-platform.com/'}
+      option :client_options,{:site => 'http://api.mixi-platform.com/',
+                              :authorize_url => 'https://mixi.jp/connect_authorize.pl',
+                              :access_token_url => 'https://secure.mixi-platform.com/2/token'
+                              }
     end
   end
 end
--- spec/omniauth/strategies/mixi_spec.rb
+++ spec/omniauth/strategies/mixi_spec.rb
@@ -11,15 +11,15 @@ describe OmniAuth::Strategies::Mixi do
     end
 
     it 'should have correct site' do
-      subject.options.client_options.site.should eq('http://api.mixi-platform.com/')
+      subject.options.client_options.site.should eq("http://api.mixi-platform.com/")
     end
 
     it 'should have authorize url' do
-      subject.options.client_options.authorize_url eq('https://mixi.jp/connect_authorize.pl')
+      subject.options.client_options.authorize_url eq("https://mixi.jp/connect_authorize.pl")
     end
     
     it 'should have access token url' do
-      subject.options.client_options.access_token_url eq('https://secure.mixi-platform.com/2/token')
+      subject.options.client_options.access_token_url eq("https://secure.mixi-platform.com/2/token")
     end
     
   end
